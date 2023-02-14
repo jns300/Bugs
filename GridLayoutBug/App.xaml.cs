fb50f5b8288588c374aae485b69d22e9647a5368
Joka <j-keller1512@wp.pl>
2023-02-14 10:02:41 +0100

    MAUI project revealing Grid layout issue.

---
+++ GridLayoutBug/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GridLayoutBug
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GridLayoutBug/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GridLayoutBug
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
