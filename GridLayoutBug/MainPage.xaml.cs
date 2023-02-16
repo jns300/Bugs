dbfb73dbd98e0221026b7a8575827c1a6a176f63
Joka <j-keller1512@wp.pl>
2023-02-16 09:29:13 +0100

    Added code showing the second case of the Grid layout bug.

--- GridLayoutBug/MainPage.xaml.cs
+++ GridLayoutBug/MainPage.xaml.cs
@@ -6,5 +6,16 @@
         {
             InitializeComponent();
         }
+        private void btnCase1_Clicked(object sender, EventArgs e)
+        {
+            gridBugCase1.IsVisible = true;
+            gridBugCase2.IsVisible = false;
+        }
+
+        private void btnCase2_Clicked(object sender, EventArgs e)
+        {
+            gridBugCase1.IsVisible = false;
+            gridBugCase2.IsVisible = true;
+        }
     }
 }
